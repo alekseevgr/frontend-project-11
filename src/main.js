7eaaefd4720396b27f5c6a2f93b8f5ed6251205d
alekseevgr <alekseevgr2@gmail.com>
2025-07-06 16:09:58 +0500

    feat: add form, styles

--- src/main.js
+++ src/main.js
@@ -1,24 +1,22 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('rssForm');
+  const input = document.getElementById('rssInput');
+  const content = document.getElementById('content');
 
-setupCounter(document.querySelector('#counter'))
+  form?.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const url = input.value.trim();
+
+    if (!url) return;
+
+    const block = document.createElement('div');
+    block.classList.add('alert', 'alert-success', 'mt-4');
+    block.textContent = `RSS успешно добавлен: ${url}`;
+    content.prepend(block);
+
+    input.value = '';
+  });
+});
