69713aa9203584ca35d9c3060a96fa00c9aec9f3
alekseevgr <alekseevgr2@gmail.com>
2026-01-12 19:52:05 +0500

    fix linter 3

--- src/parseXML.js
+++ src/parseXML.js
@@ -1,61 +1,61 @@
-import uniqueId from "lodash/uniqueId";
+import uniqueId from 'lodash/uniqueId'
 
-export default function parseXML (xml) {
+export default function parseXML(xml) {
 
-    const parser = new DOMParser();
+  const parser = new DOMParser()
 
-    const doc = parser.parseFromString(
-        xml,
-        "text/xml"
-    );
+  const doc = parser.parseFromString(
+    xml,
+    'text/xml',
+  )
 
-    const errorNode = doc.querySelector("parsererror");
-    if (errorNode) {
+  const errorNode = doc.querySelector('parsererror')
+  if (errorNode) {
 
-        throw new Error("Ошибка парсинга XML");
+    throw new Error('Ошибка парсинга XML')
 
+  }
+
+  const channel = doc.querySelector('channel')
+
+  if (!channel) {
+
+    console.log('')
+    throw new Error('Ошибка парсинга XML')
+
+  }
+  const titleFeed = channel.querySelector('title')?.textContent ?? ''
+  const descFeed = channel.querySelector('description')?.textContent ?? ''
+
+  const feed = {
+    titleFeed,
+    descFeed,
+    'id': uniqueId(),
+    'posts': [],
+  }
+
+  const items = doc.querySelectorAll('item')
+
+  items.forEach(item => {
+
+    const title = item.querySelector('title')?.textContent ?? ''
+    const link = item.querySelector('link')?.textContent ?? ''
+    const description = item.querySelector('description')?.textContent ?? ''
+    const id = feed.id
+    const postId = uniqueId()
+    const post = {
+      title,
+      link,
+      description,
+      'id': postId,
+      'idFeed': id,
+      'read': false,
     }
 
-    const channel = doc.querySelector("channel");
+    feed.posts.push(post)
 
-    if (!channel) {
+  })
 
-        console.log("");
-        throw new Error("Ошибка парсинга XML");
-
-    }
-    const titleFeed = channel.querySelector("title")?.textContent ?? "";
-    const descFeed = channel.querySelector("description")?.textContent ?? "";
-
-    const feed = {
-        titleFeed,
-        descFeed,
-        "id": uniqueId(),
-        "posts": []
-    };
-
-    const items = doc.querySelectorAll("item");
-
-    items.forEach((item) => {
-
-        const title = item.querySelector("title")?.textContent ?? "";
-        const link = item.querySelector("link")?.textContent ?? "";
-        const description = item.querySelector("description")?.textContent ?? "";
-        const id = feed.id;
-        const postId = uniqueId();
-        const post = {
-            title,
-            link,
-            description,
-            "id": postId,
-            "idFeed": id,
-            "read": false
-        };
-
-        feed.posts.push(post);
-
-    });
-
-    return feed;
+  return feed
 
 }
--- src/view.js
+++ src/view.js
@@ -1,288 +1,288 @@
-import * as yup from "yup";
-import {proxy, subscribe, snapshot} from "valtio/vanilla";
-import keyBy from "lodash.keyby";
-import i18next from "i18next";
-import axios from "axios";
-import buildPath from "./buildPath";
-import parseXML from "./parseXML";
-import render from "./renderFeeds";
+import * as yup from 'yup'
+import {proxy, subscribe, snapshot} from 'valtio/vanilla'
+import keyBy from 'lodash.keyby'
+import i18next from 'i18next'
+import axios from 'axios'
+import buildPath from './buildPath'
+import parseXML from './parseXML'
+import render from './renderFeeds'
 
-const initI18n = async () => {
+const initI18n = async() => {
 
-    await i18next.init({
-        "lng": "ru",
-        "resources": {
-            "ru": {"translation": await import("../locales/ru/translation.json")},
-            "en": {"translation": await import("../locales/en/translation.json")}
-        }
-    });
-
-};
-
-yup.setLocale({
-    "mixed": {
-        "required": () => i18next.t("form.errors.required")
+  await i18next.init({
+    'lng': 'ru',
+    'resources': {
+      'ru': {'translation': await import('../locales/ru/translation.json')},
+      'en': {'translation': await import('../locales/en/translation.json')},
     },
-    "string": {
-        "url": () => i18next.t("form.errors.url")
-    }
-});
-
-const makeSchema = (state) => yup.object({
-    "website": yup.
-        string().
-        url().
-        required().
-        test(
-            "unique",
-            () => i18next.t("form.errors.notUnique"),
-            (value) => !state.websites.includes(value)
-        )
-});
-
-const state = proxy({
-    "websites": [],
-    "errors": {},
-    "formState": "filling", // 'success' 'invalid'
-    "content": []
-});
-
-const validate = (fields, state) => {
-
-    try {
-
-        makeSchema(state).validateSync(
-            fields,
-            {"abortEarly": false}
-        );
-        return {};
-
-    } catch (e) {
-
-        return keyBy(
-            e.inner,
-            "path"
-        );
-
-    }
-
-};
-
-const elements = {
-    "label": document.querySelector("label[for=\"url-input\"]"),
-    "input": document.getElementById("url-input"),
-    "button": document.querySelector(".rss-form button[type=\"submit\"]"),
-    "error": document.querySelector(".feedback"),
-    "feeds": document.querySelector(".feeds"),
-    "posts": document.querySelector(".posts")
-};
-
-function updateTexts () {
-
-    elements.label.textContent = i18next.t("form.label");
-    elements.input.placeholder = i18next.t("form.placeholder");
-    elements.button.textContent = i18next.t("form.submit");
+  })
 
 }
 
-export default async function app () {
+yup.setLocale({
+  'mixed': {
+    'required': () => i18next.t('form.errors.required'),
+  },
+  'string': {
+    'url': () => i18next.t('form.errors.url'),
+  },
+})
 
-    await initI18n();
-    updateTexts();
-    const form = document.querySelector(".rss-form");
-    const input = document.querySelector("#url-input");
+const makeSchema = state => yup.object({
+  'website': yup.
+    string().
+    url().
+    required().
+    test(
+      'unique',
+      () => i18next.t('form.errors.notUnique'),
+      value => !state.websites.includes(value),
+    ),
+})
 
-    const defaultLang = navigator.language.startsWith("en")
-        ? "en"
-        : "ru";
-    await i18next.changeLanguage(defaultLang);
+const state = proxy({
+  'websites': [],
+  'errors': {},
+  'formState': 'filling', // 'success' 'invalid'
+  'content': [],
+})
 
-    const updateRss = (url) => {
+const validate = (fields, state) => {
 
-        const path = buildPath(url);
-        let isRetry = true;
-        axios.
-            get(path).
-            then((response) => {
+  try {
 
-                const contents = response.data.contents;
-                const parsedFeed = parseXML(contents);
+    makeSchema(state).validateSync(
+      fields,
+      {'abortEarly': false},
+    )
+    return {}
 
-                const existingFeed = state.content.find((f) => f.url === url);
+  } catch(e) {
 
-                const newPosts = parsedFeed.posts.filter((post) => {
+    return keyBy(
+      e.inner,
+      'path',
+    )
 
-                    if (!existingFeed) return true;
+  }
 
-                    return !existingFeed.posts.some((p) => p.link === post.link);
+}
 
-                });
+const elements = {
+  'label': document.querySelector('label[for="url-input"]'),
+  'input': document.getElementById('url-input'),
+  'button': document.querySelector('.rss-form button[type="submit"]'),
+  'error': document.querySelector('.feedback'),
+  'feeds': document.querySelector('.feeds'),
+  'posts': document.querySelector('.posts'),
+}
 
-                if (!existingFeed) {
+function updateTexts() {
 
-                    parsedFeed.url = url;
-                    state.content.push(parsedFeed);
-                    state.websites.push(url);
+  elements.label.textContent = i18next.t('form.label')
+  elements.input.placeholder = i18next.t('form.placeholder')
+  elements.button.textContent = i18next.t('form.submit')
 
-                } else if (newPosts.length > 0) {
+}
 
-                    existingFeed.posts.unshift(...newPosts);
+export default async function app() {
 
-                }
+  await initI18n()
+  updateTexts()
+  const form = document.querySelector('.rss-form')
+  const input = document.querySelector('#url-input')
 
-                elements.feeds.innerHTML = "";
-                elements.posts.innerHTML = "";
-                render(
-                    state,
-                    elements.feeds,
-                    elements.posts
-                );
-                input.value = "";
-                state.formState = "success";
-                isRetry = true;
+  const defaultLang = navigator.language.startsWith('en')
+    ? 'en'
+    : 'ru'
+  await i18next.changeLanguage(defaultLang)
 
-            }).
-            catch((err) => {
+  const updateRss = url => {
 
-                if (err.message === "Ошибка парсинга XML") {
+    const path = buildPath(url)
+    let isRetry = true
+    axios.
+      get(path).
+      then(response => {
 
-                    state.errors = {
-                        "website": {"message": i18next.t("form.errors.invalidRss")}
-                    };
-                    state.formState = "invalid";
-                    isRetry = false;
-                    return;
+        const contents = response.data.contents
+        const parsedFeed = parseXML(contents)
 
-                }
-                if (err.response?.status === 404) {
+        const existingFeed = state.content.find(f => f.url === url)
 
-                    state.errors = {
-                        "website": {"message": i18next.t("form.errors.notFound")}
-                    };
-                    state.formState = "invalid";
-                    isRetry = false;
-                    return;
+        const newPosts = parsedFeed.posts.filter(post => {
 
-                }
-                state.errors = {
-                    "website": {"message": i18next.t("form.errors.network")}
-                };
-                state.formState = "invalid";
-                isRetry = true;
+          if (!existingFeed) return true
 
-            }).
-            finally(() => {
+          return !existingFeed.posts.some(p => p.link === post.link)
 
-                if (isRetry) {
+        })
 
-                    setTimeout(
-                        () => updateRss(url),
-                        5000
-                    );
+        if (!existingFeed) {
 
-                }
+          parsedFeed.url = url
+          state.content.push(parsedFeed)
+          state.websites.push(url)
 
-            });
+        } else if (newPosts.length > 0) {
 
-    };
+          existingFeed.posts.unshift(...newPosts)
 
-    subscribe(
+        }
+
+        elements.feeds.innerHTML = ''
+        elements.posts.innerHTML = ''
+        render(
+          state,
+          elements.feeds,
+          elements.posts,
+        )
+        input.value = ''
+        state.formState = 'success'
+        isRetry = true
+
+      }).
+      catch(err => {
+
+        if (err.message === 'Ошибка парсинга XML') {
+
+          state.errors = {
+            'website': {'message': i18next.t('form.errors.invalidRss')},
+          }
+          state.formState = 'invalid'
+          isRetry = false
+          return
+
+        }
+        if (err.response?.status === 404) {
+
+          state.errors = {
+            'website': {'message': i18next.t('form.errors.notFound')},
+          }
+          state.formState = 'invalid'
+          isRetry = false
+          return
+
+        }
+        state.errors = {
+          'website': {'message': i18next.t('form.errors.network')},
+        }
+        state.formState = 'invalid'
+        isRetry = true
+
+      }).
+      finally(() => {
+
+        if (isRetry) {
+
+          setTimeout(
+            () => updateRss(url),
+            5000,
+          )
+
+        }
+
+      })
+
+  }
+
+  subscribe(
+    state,
+    () => {
+
+      const snap = snapshot(state)
+      const textError = snap.errors.website?.message ?? null
+      if (textError) {
+
+        elements.error.textContent = textError
+        elements.error.classList.remove('text-success')
+        elements.error.classList.add('text-danger')
+
+        elements.input.classList.add('is-invalid')
+
+      } else if (snap.formState === 'success') {
+
+        elements.error.textContent = i18next.t('form.success')
+        elements.error.classList.remove('text-danger')
+        elements.error.classList.add('text-success')
+        elements.input.classList.remove('is-invalid')
+
+      } else {
+
+        elements.error.textContent = ''
+        elements.error.classList.remove(
+          'text-danger',
+          'text-success',
+        )
+        elements.input.classList.remove('is-invalid')
+        input.focus()
+
+      }
+
+    },
+  )
+
+  i18next.on(
+    'languageChanged',
+    updateTexts,
+  )
+
+  form.addEventListener(
+    'submit',
+    e => {
+
+      e.preventDefault()
+      const data = new FormData(e.target)
+      const url = data.get('url')
+
+      const errors = validate(
+        {'website': url},
         state,
-        () => {
+      )
+      state.errors = errors
 
-            const snap = snapshot(state);
-            const textError = snap.errors.website?.message ?? null;
-            if (textError) {
+      if (Object.keys(errors).length === 0) {
 
-                elements.error.textContent = textError;
-                elements.error.classList.remove("text-success");
-                elements.error.classList.add("text-danger");
+        state.formState = 'sending'
+        updateRss(url)
 
-                elements.input.classList.add("is-invalid");
+      } else {
 
-            } else if (snap.formState === "success") {
+        state.formState = 'invalid'
 
-                elements.error.textContent = i18next.t("form.success");
-                elements.error.classList.remove("text-danger");
-                elements.error.classList.add("text-success");
-                elements.input.classList.remove("is-invalid");
+      }
 
-            } else {
+    },
+  )
 
-                elements.error.textContent = "";
-                elements.error.classList.remove(
-                    "text-danger",
-                    "text-success"
-                );
-                elements.input.classList.remove("is-invalid");
-                input.focus();
+  elements.posts.addEventListener(
+    'click',
+    e => {
 
-            }
+      if (!e.target.matches('button')) return
 
-        }
-    );
+      const postId = e.target.dataset.id
+      const post = state.content.
+        flatMap(f => f.posts).
+        find(p => p.id === postId)
 
-    i18next.on(
-        "languageChanged",
-        updateTexts
-    );
+      if (!post) return
 
-    form.addEventListener(
-        "submit",
-        (e) => {
+      post.read = true
 
-            e.preventDefault();
-            const data = new FormData(e.target);
-            const url = data.get("url");
+      const link = elements.posts.querySelector(`a[data-id="${postId}"]`)
+      if (link) {
 
-            const errors = validate(
-                {"website": url},
-                state
-            );
-            state.errors = errors;
+        link.classList.remove('fw-bold')
+        link.classList.add('fw-normal')
 
-            if (Object.keys(errors).length === 0) {
+      }
 
-                state.formState = "sending";
-                updateRss(url);
+      document.querySelector('.modal-title').textContent = post.title
+      document.querySelector('.modal-body').textContent = post.description
+      document.querySelector('.full-article').href = post.link
 
-            } else {
-
-                state.formState = "invalid";
-
-            }
-
-        }
-    );
-
-    elements.posts.addEventListener(
-        "click",
-        (e) => {
-
-            if (!e.target.matches("button")) return;
-
-            const postId = e.target.dataset.id;
-            const post = state.content.
-                flatMap((f) => f.posts).
-                find((p) => p.id === postId);
-
-            if (!post) return;
-
-            post.read = true;
-
-            const link = elements.posts.querySelector(`a[data-id="${postId}"]`);
-            if (link) {
-
-                link.classList.remove("fw-bold");
-                link.classList.add("fw-normal");
-
-            }
-
-            document.querySelector(".modal-title").textContent = post.title;
-            document.querySelector(".modal-body").textContent = post.description;
-            document.querySelector(".full-article").href = post.link;
-
-        }
-    );
+    },
+  )
 
 }
